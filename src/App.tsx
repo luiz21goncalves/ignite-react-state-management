374bbacfb2debf7f7a08b044e145eb11e7c6945e
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-06-24 13:04:17 -0300

    chore: add tailwindcss config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Provider as ReduxProvider } from 'react-redux'
 
+import { Player } from './pages/Player'
 import { store } from './store'
+
+import './styles/global.css'
 
 export function App() {
   return (
     <ReduxProvider store={store}>
-      <h1>app</h1>
+      <Player />
     </ReduxProvider>
   )
 }
---
+++ src/pages/Player/index.tsx
@@ -0,0 +1,7 @@
+export function Player() {
+  return (
+    <div className="flex h-screen items-center  justify-center bg-zinc-950 text-zinc-50">
+      Player
+    </div>
+  )
+}
