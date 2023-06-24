45b7437077eaf33b2a39c6407c02d07c5883fdf3
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-06-24 12:08:07 -0300

    feat: add new todo to store

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,7 +1,30 @@
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { add } from '../store'
+
 export function AddTodo() {
+  const [newTodo, setNewTodo] = useState('')
+  const dispatch = useDispatch()
+
+  function handleAddTodo(event: FormEvent) {
+    event.preventDefault()
+
+    dispatch(add({ newTodo }))
+  }
+
+  function onChangeNewTodo(event: ChangeEvent<HTMLInputElement>) {
+    setNewTodo(event.target.value)
+  }
+
   return (
-    <form>
-      <input type="text" placeholder="Novo to-do" />
+    <form onSubmit={handleAddTodo}>
+      <input
+        type="text"
+        placeholder="Novo to-do"
+        value={newTodo}
+        onChange={onChangeNewTodo}
+      />
       <button type="submit">Adicionar</button>
     </form>
   )
--- src/store/index.ts
+++ src/store/index.ts
@@ -3,8 +3,14 @@ import { configureStore, createSlice } from '@reduxjs/toolkit'
 const todoSlice = createSlice({
   name: 'todo',
   initialState: ['Fazer café', 'Estudar Redux'],
-  reducers: {},
+  reducers: {
+    add: (state, action) => {
+      state.push(action.payload.newTodo)
+    },
+  },
 })
+
+export const { add } = todoSlice.actions
 
 export const store = configureStore({
   reducer: {
